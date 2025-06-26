304ef41621b64414a471bbef2a8dc35a0394e1e5
fatihjoe <fatihjoe@gmail.com>
2025-06-26 09:29:20 +0300

    SENSOR_DELAY_FASTEST

--- src/android/StepIstListener.java
+++ src/android/StepIstListener.java
@@ -144,7 +144,8 @@ public class StepIstListener extends CordovaPlugin implements SensorEventListene
         // If found, then register as listener
         if ((list != null) && (list.size() > 0)) {
             this.mSensor = list.get(0);
-            if (this.sensorManager.registerListener(this, this.mSensor, SensorManager.)) {
+            boolean isSupported = this.sensorManager.registerListener(this, this.mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            if (isSupported) {
                 this.setStatus(StepIstListener.STARTING);
             } else {
                 this.setStatus(StepIstListener.ERROR_FAILED_TO_START);
